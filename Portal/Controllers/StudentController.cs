9b45537339b2ee2c83c4c2fbd8d6b1cdd6c42af3
Esther Timothy <etimothy@axxess.com>
2023-05-12 18:52:34 +0100

    Added Post ActionResult for reg form submission

--- Portal/Controllers/StudentController.cs
+++ Portal/Controllers/StudentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Portal.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,16 +9,70 @@ namespace Portal.Controllers
 {
     public class StudentController : Controller
     {
+        PortalDBEntities db = new PortalDBEntities();
+
         // GET: Student
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult SignUp()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult SignUp(StudentTable signup)
+        {
+            //Validation
+            if (ModelState.IsValid)
+            {
+                //check if password == confirm password 
+                if (signup.Password != signup.ConfirmPassword)
+                {
+                    ViewBag.Error = "Password does not match";
+                    return View(signup);
+                }
+                else
+                {
+                    //check if matric number already exists
+                    var userCheck = db.StudentTables.Where(x => x.MatricNo == signup.MatricNo).FirstOrDefault();
+
+                    if (userCheck != null)
+                    {
+                        ViewBag.Error = "Student already registered";
+                        return View(signup);
+                    }
+                    else
+                    {
+                        //Process form
+                        //submit form
+                        db.StudentTables.Add(signup);
+                        try
+                        {
+                            db.SaveChanges();
+                            return RedirectToAction("Dashboard", signup);
+                        }
+                        catch
+                        {
+                            ViewBag.Error = "Registration Unsuccessful.";
+                            return View(signup);
+                        }
+                    }
+
+                    return View();
+                }
+            }
+            else
+            {
+                return View(signup);
+            }
+            
+        }
+
+
 
         public ActionResult Login()
         {
